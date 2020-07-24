47c2d0b77863a8bc5b5db9e71933e964a50265af
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-24 14:40:00 +0200

    Add front-end from Cody

---
+++ src/frontend.cody/components/Navigation/NavBarClone.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav id="navbar-clone" :class="navBarCloneClass">
+    <div class="container">
+      <!-- Brand -->
+      <div class="navbar-brand">
+        <a href="index.html" class="navbar-item">
+          <img src="@/assets/img/logo/2ndlayer-logo.svg" alt="">
+        </a>
+        <ResponsiveToggle/>
+      </div>
+      <!-- Menu -->
+      <div id="cloneNavbarMenu" :class="navMenuClass">
+        <NavContent/>
+      </div>
+    </div>
+  </nav>
+  <!-- /Cloned navbar -->
+</template>
+
+<script>
+  import NavContent from "./NavContent";
+  import ResponsiveToggle from "./ResponsiveToggle";
+  import {mapState} from 'vuex';
+
+  export default {
+    components: {
+      NavContent,
+      ResponsiveToggle
+    },
+    computed: {
+      navBarCloneClass() {
+        return ['navbar', 'is-fixed', 'is-dark', this.scrollPosition > 50 || this.isMobileMenuToggled ? 'is-active' : ''];
+      },
+      navMenuClass() {
+        return ['navbar-menu', this.isMobileMenuToggled ? 'is-active' : '']
+      },
+      ...mapState('responsive-toggle', ['isMobileMenuToggled'])
+    },
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    data() {
+      return {
+        scrollPosition: 0
+      };
+    },
+    destroyed() {
+      window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+      handleScroll(event) {
+        this.scrollPosition = window.top.scrollY;
+      }
+    }
+  }
+</script>
