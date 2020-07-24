88d99acf9b62cf059db958b89e7211cfb620d89e
Cody Butz <cbutz@omnispear.com>
2020-07-24 18:00:47 -0400

    Cody v2 changes.

--- src/frontend/components/footer.vue
+++ src/frontend/components/footer.vue
@@ -1,16 +1,64 @@
 <template>
-  <footer class="footer has-background-secondary">
+  <footer class="footer">
+    <div class="container">
+      <!-- Logo -->
+      <div class="footer-logo">
+        <a href="#">
+          <img src="@/assets/images/2ndLayer-logo.svg" alt="">
+          <div class="brand-subtitle">Multi-Ledger Staking Services</div>
+        </a>
+      </div>
+
+      <!-- Columns -->
+      <div class="columns footer-columns is-vcentered">
+        <div class="column is-4">
+          <!-- Links group -->
+          <ul class="footer-links">
+            <li>
+              <a href="/cardano-staking-pool">Cardano Staking Pool</a>
+            </li>
+
+            <li>
+              <a href="/tezos-bakery">Tezos Bakery</a>
+            </li>
+
+          </ul>
+        </div>
+        <!-- Newsletter -->
+        <div class="column is-4">
+          <div class="subscribe-block">
+            <form>
+              <!-- Field -->
+              <div class="control">
+                <!-- Special input -->
+                <input class="krypton-subscribe-input" type="email" name="email" placeholder="">
+                <button class="subscribe-button">
+                  <span>Subscribe</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- Links group -->
+        <div class="column is-4">
+          <ul class="footer-links">
+            <li>
+              <a href="https://twitter.com/2nd_Layer">Twitter</a>
+            </li>
+
+            <li>
+              <a href="https://www.youtube.com/channel/UCncyGaCH5emsxroa3-YIb_A/">YouTube</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Copyright -->
+      <p class="k-copyright">© 2020 | 2nd Layer. All Rights Reserved</p>
+      <br>
+    </div>
+
+    <!-- Absolute image -->
+    <img class="solar-system" src="@/assets/images/bg/solar.svg" alt="">
   </footer>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
-<style>
-footer.footer {
-  padding-bottom: 1rem;
-}
-</style>
---
+++ src/frontend/layouts/landing.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="landing-wrapper">
+    <particles-bg type="cobweb" :bg="false" :color="color" style="position: absolute; top: 0; bottom: 0;"/>
+    <div class="hero is-fullheight">
+      <div class="hero-header">
+        <Header/>
+      </div>
+      <div class="hero-body">
+        <nuxt/>
+      </div>
+      <div class="hero-footer">
+        <!-- Hero footer -->
+        <div class="hero-foot">
+          <div class="container">
+            <div class="is-centered coin-list">
+              <a href="https://cardano.org/" target="_blank">
+                <img class="hero-logo" src="@/assets/images/coins/Cardano-coin-ADA-symbol.svg" alt="Cardano">
+              </a>
+              <a href="#">
+                <img class="hero-logo" src="@/assets/images/coins/Tezos-coin-XTZ-symbol.svg" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        color: ["#5507fc"]
+      };
+    },
+    head() {
+      return {
+        title: '2ⁿᵈ Layer Staking Services'
+      }
+    }
+  }
+</script>
