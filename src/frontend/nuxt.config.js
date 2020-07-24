da1bb1f45217b8154dd19a4c1c28cebd0c7de29e
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-24 16:40:27 +0200

    Update nuxt.config.js

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -1,5 +1,20 @@
 export default {
     target: 'static', // default: 'server'
+    /*
+    ** Headers of the page
+    ** See https://nuxtjs.org/api/configuration-head
+    */
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
     modules: [
         'nuxt-buefy',
         '@nuxtjs/sitemap',
