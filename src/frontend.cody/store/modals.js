f3d27d638ba087fe2524275c42b1dc2945a6c132
Cody Butz <cbutz@omnispear.com>
2020-07-28 19:36:07 -0400

    Fix images.

--- src/frontend.cody/store/modals.js
+++
@@ -1,9 +0,0 @@
-export const state = () => ({
-  isGetStartedModalToggled: false
-});
-
-export const mutations = {
-  toggleGetStartedModal (state) {
-    state.isGetStartedModalToggled = !state.isGetStartedModalToggled;
-  }
-}
--- src/frontend.cody/store/responsive-toggle.js
+++
@@ -1,9 +0,0 @@
-export const state = () => ({
-  isMobileMenuToggled: false
-});
-
-export const mutations = {
-  toggle (state) {
-    state.isMobileMenuToggled = !state.isMobileMenuToggled;
-  }
-}
